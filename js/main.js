db13ec7bb8f3a98d80737a76700c161fe918422c
Nikolay Golovin <nik.golovin83@gmail.com>
2020-09-29 17:49:37 +0600

    задание выполнено

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const profiles = [];
 const profilesNum = 25;
 const userNames = [`Вася`, `Лена`, `Слава`, `Толя`, `Оля`, `Катя`];
-
-let messages = [`Всё отлично!`,
+const messages = [`Всё отлично!`,
   `В целом всё неплохо. Но не всё.`,
   `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
   `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
@@ -12,6 +10,19 @@ let messages = [`Всё отлично!`,
   `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
 ];
 
+const getAvatar = () => `img/avatar-${getRandom(0, 6)}.svg`;
+const getName = () => userNames[getRandom(0, userNames.length - 1)];
+const getComments = (imgIndx) => {
+  const temp = [];
+  for (let i = 0; i < imgIndx * getRandom(0, 100); i++) {
+    temp.push({
+      avatar: getAvatar(),
+      name: getName(),
+      messages: messages[getRandom(0, messages.length - 1)]
+    });
+  }
+  return temp;
+};
 
 const usersImg = document.querySelector('.pictures');
 const template = document.querySelector('#picture').content.querySelector('.picture');
@@ -19,25 +30,27 @@ const template = document.querySelector('#picture').content.querySelector('.pict
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getProfiles = () => {
+  const temp = [];
   for (let i = 0; i < profilesNum; i++) {
-    profiles.push({
+    temp.push({
       url: `photos/${i + 1}.jpg`,
-      avatar: `img/avatar-${getRandom(0, 6)}.svg`,
-      name: userNames[getRandom(0, userNames.length - 1)],
+      avatar: getAvatar(),
+      name: getName(),
       likes: getRandom(15, 200),
-      comments: messages[getRandom(0, messages.length - 1)]
+      comments: getComments((i + 1))
     });
   }
+  return temp;
 };
 
-getProfiles();
+const profiles = getProfiles();
 
 const getProfileInf = (profileParam) => {
 
   let profileElement = template.cloneNode(true);
 
   profileElement.querySelector('.picture__img').src = profileParam.url;
-  profileElement.querySelector('.picture__comments').textContent = profileParam.comments;
+  profileElement.querySelector('.picture__comments').textContent = profileParam.comments.length;
   profileElement.querySelector('.picture__likes').textContent = profileParam.likes;
 
   return profileElement;

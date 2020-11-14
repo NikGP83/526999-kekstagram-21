e0b565aee428df85ebf5ccbedebc261c822a4775
Nikolay Golovin <nik.golovin83@gmail.com>
2020-11-14 16:08:59 +0600

    начало работы с модулями

--- js/main.js
+++ js/main.js
@@ -26,29 +26,12 @@ const regExTagCandidate = /^#[a-z\d]+$/g;
 
 const profilesNum = 25;
 const userNames = [`Вася`, `Лена`, `Слава`, `Толя`, `Оля`, `Катя`];
-const messages = [`Всё отлично!`,
-  `В целом всё неплохо. Но не всё.`,
-  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
-  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
-  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
-];
 
-const getAvatar = () => `img/avatar-${getRandom(0, 6)}.svg`;
-const getName = () => userNames[getRandom(0, userNames.length - 1)];
-const getComments = (imgIndx) => {
-  const temp = [];
-  for (let i = 0; i < imgIndx * getRandom(0, 100); i++) {
-    temp.push({
-      avatar: getAvatar(),
-      name: getName(),
-      messages: messages[getRandom(0, messages.length - 1)]
-    });
-  }
-  return temp;
-};
+const getAvatar = () => `img/avatar-${window.getRandom(0, 6)}.svg`;
+const getName = () => userNames[window.getRandom(0, userNames.length - 1)];
 
-const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getProfiles = () => {
   const temp = [];
@@ -57,8 +40,8 @@ const getProfiles = () => {
       url: `photos/${i + 1}.jpg`,
       avatar: getAvatar(),
       name: getName(),
-      likes: getRandom(15, 200),
-      comments: getComments((i + 1))
+      likes: window.getRandom(15, 200),
+      comments: window.data.getComments((i + 1))
     });
   }
   return temp;
---
+++ js/util.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(()=> {
+  window.getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+})();
